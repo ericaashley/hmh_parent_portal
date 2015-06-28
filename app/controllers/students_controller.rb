a1966bbb2d8694ce565972cff9c7f58cd3900669
emanoppo <emanoppo@users.noreply.github.com>
2015-06-28 12:41:21 -0400

    Add additional parent views

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,4 +3,10 @@ class StudentsController < ApplicationController
 		@section = Section.find(params[:section_id])
 		@students = @section.students
 	end
+
+	def show
+		@student = Student.find(params[:id])
+		@student_sections = @student.sections
+		@assignment_submissions = @student.assignment_submissions
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   resources :students, only: [:show]
 
+  resources :assignment_submissions, only: [:show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
