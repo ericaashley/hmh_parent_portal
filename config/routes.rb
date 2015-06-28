b4b3196b715bc27491e415008712852759095993
Rob Monk <robert.monk85@gmail.com>
2015-06-27 23:19:36 -0400

    Ram views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'assignments#index'
+  root 'homes#landing'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
