cd94823dc2488fcba2a105a5bb83f7740e33a7d0
Matthew Riggott <Matthew.S.Riggott@gmail.com>
2015-06-28 13:16:41 -0400

    comments in the ouse

--- app/controllers/assignment_submissions_controller.rb
+++ app/controllers/assignment_submissions_controller.rb
@@ -1,3 +1,9 @@
 class AssignmentSubmissionsController < ApplicationController
-
+  def show
+    @submission = AssignmentSubmission.find(params[:id])
+    @student = @submission.student
+    @assignment = @submission.assignment
+    @section = @assignment.section
+    @teacher = @assignment.staff_person
+  end
 end
