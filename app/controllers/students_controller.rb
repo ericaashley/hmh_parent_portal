47e85b613b444c6e3c3d1defab7295bab961d071
emanoppo <emanoppo@users.noreply.github.com>
2015-06-28 11:26:49 -0400

    Add views

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,6 @@
 class StudentsController < ApplicationController
-
+	def index
+		@section = Section.find(params[:section_id])
+		@students = @section.students
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root 'assignments#index'
   devise_for :users
-  
+
   resources :sections, only: [:index] do
-    resources :students, only: [:show]
+    resources :students, only: [:index]
   end
+
+  resources :students, only: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
